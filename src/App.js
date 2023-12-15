c2bb0246ebe7c13c053d0b0178d539e82a698071
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2023-12-15 12:01:02 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import './App.css'; // Style dosyanızın adına göre import edin
+
+function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <div className={`container ${isActive ? 'active' : ''}`} id="container">
+      {/* Sign Up Form */}
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email for registeration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="button" onClick={handleRegisterClick}>Sign Up</button>
+        </form>
+      </div>
+      
+      {/* Sign In Form */}
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
+            <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forget Your Password?</a>
+          <button type="button" onClick={handleLoginClick}>Sign In</button>
+        </form>
+      </div>
+
+      {/* Toggle Button */}
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className={`toggle-panel toggle-left ${isActive ? 'active' : ''}`}>
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all site features</p>
+            <button className="hidden" id="login" onClick={handleLoginClick}>Sign In</button>
+          </div>
+          <div className={`toggle-panel toggle-right ${isActive ? '' : 'active'}`}>
+            <h1>Hello, Friend!</h1>
+            <p>Register with your personal details to use all site features</p>
+            <button className="hidden" id="register" onClick={handleRegisterClick}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
